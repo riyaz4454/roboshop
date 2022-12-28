e8dc5428aa32a3eeed8730f1367e7e852ea44017
riyaz shaik <526473+gb96@users.noreply.github.com>
2022-12-28 21:50:44 +0000

    update changes in frontend

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -20,6 +20,13 @@ Head "Unzip Downloaded Archive"
 cd /var/www && unzip -o /tmp/frontend.zip &>>$LOG && mv frontend-main/* . && mv static html && rm -rf frontend-main README.md
 Stat $?
 
+Head "Update Nginx Configuration"
+mv roboshop.conf /etc/nginx/sites-enabled/roboshop.conf
+for comp in catalogue cart user shipping payment ; do
+  sed -i -e "/$comp/ s/localhost/${comp}.rsdevops01.tk/" /etc/nginx/sites-enabled/roboshop.conf
+done
+Stat $?
+
 Head "Restart Nginx Service"
 systemctl restart nginx
 Stat $?
